844cdd16c10badda60fa1f97f184d78e8398a4e4
Pedro Godoy <pedro.godoy.simon@gmail.com>
2023-06-22 17:35:58 -0300

    Remove dummy expense data

--- src/app.js
+++ src/app.js
@@ -10,19 +10,10 @@ import { setTextFilter } from "./actions/filters";
 import getVisibleExpenses from "./selectors/expenses";
 import "normalize.css/normalize.css";
 import "./styles/styles.scss";
+import "./firebase/firebase";
 
 const store = configureStore();
 
-store.dispatch(addExpense({ description: "Water bill", amount: 4500 }));
-store.dispatch(addExpense({ description: "Gas bill", createdAt: 1000 }));
-store.dispatch(addExpense({ description: "Rent", amount: 109500 }));
-
-const state = store.getState();
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-console.log(visibleExpenses);
-
-// Provider serve para definir a store que será compatilhada com todos os outros componentes
-// o props que o Provider leva é a store que será compartilhada com o resto da aplicação
 const jsx = (
     <Provider store={store}>
         <AppRouter />
