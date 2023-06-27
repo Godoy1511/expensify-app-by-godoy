54dde76cfdec2de119339c4daaad1a1fcddd950b
Pedro Godoy <pedro.godoy.simon@gmail.com>
2023-06-27 18:58:02 -0300

    Setup production webpack build

---
+++ src/firebase/firebase.js
@@ -0,0 +1,15 @@
+import * as firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAkVeLNYRoxmnk4QhqyjmfpFPClA8lCA3k",
+  authDomain: "expensify-c08a0.firebaseapp.com",
+  databaseURL: "https://expensify-c08a0-default-rtdb.firebaseio.com",
+  projectId: "expensify-c08a0",
+  storageBucket: "expensify-c08a0.appspot.com",
+  messagingSenderId: "1025600946782",
+  appId: "1:1025600946782:web:b6728d74d9de3e9ea06acf",
+  measurementId: "G-5BCX33SV2B"
+};
+
+  firebase.initializeApp(firebaseConfig);
+
