4a8e3ef5b815f135ef348db88415cd99f2b35d8a
Pedro Godoy <pedro.godoy.simon@gmail.com>
2023-07-20 17:26:16 -0300

    styles updated

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,10 +2,13 @@ import React from "react";
 import { NavLink } from "react-router-dom"
 
 const Header = () => (
-    <header>
-        <h1>Expensify</h1>
-        <NavLink to="/" activeClassName="is-active" exact={true}>Dashboard</NavLink>
-        <NavLink to="/create" activeClassName="is-active">Create Expense</NavLink>
+    <header className="header">
+            <NavLink className="header__title" to="/" activeClassName="is-active" exact={true}>
+                <h1>Expensify</h1>
+                Dashboard
+            </NavLink>
+            &nbsp;&nbsp;&nbsp;
+            <NavLink className="header__title" to="/create" activeClassName="is-active">Create Expense</NavLink> 
     </header>
 );
 
